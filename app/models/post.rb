9fa79fb0999a5bee9f377c6a7d7e53891b21fa95
Imran Khan <m_imran_khan@me.com>
2016-07-24 23:59:08 +0200

    layout + basic functions

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base  
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { 
+  	:medium => "640x",
+  	:small => "80x80"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end  
